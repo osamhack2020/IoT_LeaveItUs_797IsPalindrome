ba91959a6d56a50627cd54b7186fbf67d25a550f
김상윤 <68493794+ndkim11@users.noreply.github.com>
2020-10-15 00:07:13 +0900

    Task Function Update
    Task 2, 3 : FSR& Door update
    using Time.h
    create taskhandler

--- ArduinoCode/holder.ino
+++ ArduinoCode/holder.ino
@@ -1,3 +1,4 @@
+#include <Time.h>
 #include <MFRC522.h>
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
@@ -20,23 +21,23 @@
 // Global variables and defines
 String InSerialStr = "";
 // object initialization
-FSR fsr_1(FSR_1_PIN_1);
-FSR fsr_2(FSR_2_PIN_1);
-FSR fsr_3(FSR_3_PIN_1);
-FSR fsr_4(FSR_4_PIN_1);
-FSR fsr_5(FSR_5_PIN_1);
-FSR fsr_6(FSR_6_PIN_1);
-FSR fsr_7(FSR_7_PIN_1);
+
 MFRC522 rfid(RFID_PIN_SDA,RFID_PIN_RST); //pin 53 & 3
 
-SoftwareSerial LoRaSerial(RX, TX);
-int PhoneStatus[3][7]; //O/X , weight, returned or not
+SoftwareSerial LoRaSerial(18, 19); //RX, TX
+int PhoneStatus[3][7]; // 행: 금일 영내 여부, 무게, 반납여부
 String RegisteredRFtag[7]; //string of registered RFtag
-String UID_from_rfid;
-byte door;
-// const int timeout = 10000;       //define timeout of 10 sec
-// long time0;
+String UID_from_rfid; //Info read from rfid
+byte door; //open or close
+byte UsingHourStart; //Web을 통해 불출시간 정할 수 있음
+byte UsingHourEnd; //Web을 통해 반납시간 정할 수 있음
+byte RoomNum; //생활관 번호 지정됨
 
+//define task handlers
+TaskHandle_t Task_NFC_Handler;
+TaskHandle_t Task_Check_Handler;
+TaskHandle_t Task_Door_Handler;
+TaskHandle_t Task_LoRa_Rcv_Handler;
 // 세마포어 핸들을 선언합니다.
 SemaphoreHandle_t sem;
 //------------------------------------------------------------------------------
@@ -45,9 +46,6 @@ SemaphoreHandle_t sem;
 //Thread1 : RFID를 통해 NFC tag 판단
 static void vNFC(void * arg){
   while(1){
-    // if(InSerialStr == "Reset RFID"){
-      
-    // }
     if ( ! rfid.PICC_IsNewCardPresent()) //새로운 것 들어올떄만 다음으로 넘어감
       return;
         
@@ -57,7 +55,7 @@ static void vNFC(void * arg){
     }
     UID_from_rfid.toUpperCase();
     for(byte i = 0; i < 7; i++){
-      if(PhoneStatus[1][i] == UID.substring(1)){//일치하는 폰 찾음
+      if(RegisteredRFtag[i] == UID_from_rfid.substring(1)){//일치하는 폰 찾음
         if(PhoneStatus[2][i] == 0){ //불출된 상태이면
           PhoneStatus[2][i] = 1; //반납처리됨
         }
@@ -66,37 +64,71 @@ static void vNFC(void * arg){
         }
       }
     }
+    for(byte i = 0; i < 7; i++){ //불출 반납 상황 송신
+      LoRaSerial.print(PhoneStatus[2][i]);
+      LoRaSerial.print(" ");
+    }
+    LoRaSerial.println();
   }
 }
 
 //Thread2 : FSR를 통해 정식 핸드폰을 제출했는지 판단
 static void vCheck(void * argl){
+  
+  FSR fsr1(FSR_1_PIN_1);
+  FSR fsr2(FSR_2_PIN_1);
+  FSR fsr3(FSR_3_PIN_1);
+  FSR fsr4(FSR_4_PIN_1);
+  FSR fsr5(FSR_5_PIN_1);
+  FSR fsr6(FSR_6_PIN_1);
+  FSR fsr7(FSR_7_PIN_1);
+  FSR fsr[7] = {fsr1, fsr2,fsr3,fsr4,fsr5,fsr6,fsr7};
+  byte ReturnErr[7] = {0, };
   while(1){
+    float weight;
+    //Serial.print(F("Force: ")); Serial.print(fsr_1Force); Serial.println(F(" [g]"));
+    
     for(byte i = 0; i < 7; i++){
-      if(PhoneStatus[])
+      if(PhoneStatus[0][i]){//반납 대상자이면
+        weight = fsr[i].getForce();
+        float ratio = weight/PhoneStatus[1][i];
+        if(0.9 > ratio || ratio > 1.1){//휴대폰 케이스 변경, 센서 오차 감안한 오차범위
+          ReturnedErr[i] = 1;
+        }
+      }
     }
   }
 }
 
 //Thread3 : 문이 열렸는지 안열렸는지 판단
-static void vdoor(void *arg){
+static void vDoor(void *arg){
   pinMode(DOORSWITCH, INPUT); //2번에서 값 읽어옴
-  door = digitalRead(DOORSWITCH); //High : closed low: open
+  byte unlockedHolder = 0; //휴대폰 보관함이 열려있음을 알린다.
+  
   while(1){ //AT+SEND=<Address>,<Payload Length>,<Data>
-      if (LoRaSerial.available()){
-        LoRaSerial.print(door);
-        if(LoRaSerial.read() == 1)
-      if(InSerialStr == )
+    door = digitalRead(DOORSWITCH); //High : closed low: open
+    if(door == 0){ //open
+      if(hour() < UsingHourStart && hour() > UsingHourEnd)
+        unlockedHolder = 1; //alert
+    }
+    if (LoRaSerial.available()){
+      LoRaSerial.print("door : ");
+      LoRaSerial.println(door);
+      if(unlockedHolder)  
+        LoRaSerial.println("휴대폰 보관함 문이 임의로 열렸습니다.")
     }
   }
 }
 
 //Thread4 : 로라
-static void vLoRa(void *arg){
+static void vLoRa_Receive(void *arg){
+  RoomNum = Serial.read();
   while(1){
     if(LoRaSerial.available()){
       PhoneStatus
     }
+    InSerialStr = LoRaSerial.readStringUntil('\n');
+    
   }
 
 void setup() 
@@ -105,19 +137,18 @@ void setup()
     LoRaSerial.begin(115200);
     while (!Serial); // wait for serial port to connect. Needed for native USB
     while (!Serial);
-    Serial.print("AT\r\n");
-    //delay function is needed
-
+    while(Serial.print("AT\r\n") != "OK");
+    
     Serial.print("AT+PARAMETER=10,7,1,7\r\n"); //For less than 3Kms
     //LoRaSerial.print("AT+PARAMETER = 12,3,1,7\r\n"); //For more than 3kms
     Serial.print("AT+BAND=915000000\r\n"); //Bandwidth set to 915MHz
-    //delay()
+    vTaskDelay((500L * configTICK_RATE_HZ) / 1000L);
 
     Serial.print("AT+ADDRESS=1\r\n"); //고유 주소를 가져야 한다. 부대에 따른 코드 부여: 0~65535
-    //delay()
+    vTaskDelay((500L * configTICK_RATE_HZ) / 1000L);
 
     Serial.print("AT+NETWORKID=5\r\n"); //허브와 보관함이 같아야 한다. 부대 내의 생활관 번호 부여: 0~16
-    //delay()
+    vTaskDelay((500L * configTICK_RATE_HZ) / 1000L);
 
     //initialize RFID module
     SPI.begin();
@@ -125,39 +156,12 @@ void setup()
     
     int PhoneStatus[2][7] //WEB에서 값읽어와서 업데이트해줘야함
     String RegisteredRFtag[7] //WEB에서 읽어와야 함
-}
-
-void loop() 
-{
-    if (Serial.available())
-    {
-    InSerialStr = LoRaSerial.readStringUntil('\n');
-    }
-
     
-    float fsr_1Force = fsr_1.getForce();
-    Serial.print(F("Force: ")); Serial.print(fsr_1Force); Serial.println(F(" [g]"));
+    xTaskCreate(vNFC,"RFID",128, NULL, Priority, &Task_NFC_Handler);
+    xTaskCreate(vCheck, "Check", 128, NULL, Priority, &Task_Check_Handler);
+    xTaskCreate(vDoor, "DoorStatus", 128, NULL, Priority, &Task_Door_Handler);
+    xTaskCreate(vLoRa_Receive, "LoRaReceive",128, NULL, Priority, &Task_LoRa_Rcv_Handler);
+    
 }
 
-char menu()
-{
-    while (!Serial.available());
-
-    // Read data from serial monitor if received
-    while (Serial.available()) 
-    {
-        char c = Serial.read();
-        if (isAlphaNumeric(c)) 
-        {   
-
-            else
-            {
-                Serial.println(F("illegal input!"));
-                return 0;
-            }
-            time0 = millis();
-            return c;
-        }
-    }
-}
-
+void loop() {} //NO LOOP
