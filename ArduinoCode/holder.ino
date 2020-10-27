6daf3ff4eeba057f642b05c6872f054eff542e02
김상윤 <68493794+ndkim11@users.noreply.github.com>
2020-10-27 21:21:57 +0900

    gateway로 송신하는 코드 tast 4로 합침
    일괄 송신을 통해 message loss 확률 줄임

--- ArduinoCode/holder.ino
+++ ArduinoCode/holder.ino
@@ -17,8 +17,6 @@
 #define RFID_PIN_RST	3 
 #define RFID_PIN_SDA	53 
 
-// Global variables and defines
-String InSerialStr = "";
 
 MFRC522 rfid(RFID_PIN_SDA,RFID_PIN_RST); //pin 53 & 3
 
@@ -26,18 +24,19 @@ SoftwareSerial LoRaSerial(18, 19); //RX, TX
 int PhoneStatus[2][7]; // 행: 0.무게, 1.반납여부
 //String RegisteredRFtag[7]; //string of registered RFtag
 String UID_from_rfid; //Info read from rfid
-byte door; //open or close
-bool usingTime;
+byte door; //휴대폰 보관함이 열려있음을 알린다.
 bool checkActiv;
-String LockerUID = ;//보관함 UID를 ASCII 문자열로 EEPROM에 저장
+String LockerUID = abcdef;//보관함 UID를 ASCII 문자열로 EEPROM에 저장
+byte UID_length= LockerUID.length(); //UID의 길이/바이트 수
+
 
 //define task handlers
 TaskHandle_t Task_NFC_Handler;
 TaskHandle_t Task_Check_Handler;
 TaskHandle_t Task_Door_Handler;
-TaskHandle_t Task_LoRa_Rcv_Handler;
+TaskHandle_t Task_LoRa_Handler;
 
-//rfid, fsr, door, lora 수행함
+//rfid, fsr, door, lora 입출력 수행함
 //Thread1 : RFID를 통해 NFC tag 판단
 static void vNFC(void * arg){
   while(1){
@@ -61,23 +60,11 @@ static void vNFC(void * arg){
       }
       
     }
-    if (runEvery(random(10000,20000))){ //10sec ~ 20sec 사이에서 임의로 전송
-      LoRaSerial.print("AT+SEND=1,11,LockerUID :");
-      LoRaSerial.print(LockerUID);
-      LoRaSerial.print("AT+SEND=1,28,");
-      Serial.print("LoRa status: Sending message to gateway"); //debugging 용이하도록 시리얼 출력
-      for(byte i = 0; i < 7; i++){ //불출 반납 상황 송신
-        LoRaSerial.print(PhoneStatus[2][i]);
-        LoRaSerial.print(" ");
-      }
-      LoRaSerial.println();
-      LoRaSerial.println("AT+SEND=1,11,END");
-    }
   }
 }
 
 //Thread2 : FSR를 통해 정식 핸드폰을 제출했는지 판단
-static void vCheck(void * argl){
+static void vCheck(void * arg){
   
   FSR fsr1(FSR_1_PIN_1);
   FSR fsr2(FSR_2_PIN_1);
@@ -87,7 +74,7 @@ static void vCheck(void * argl){
   FSR fsr6(FSR_6_PIN_1);
   FSR fsr7(FSR_7_PIN_1);
   FSR fsr[7] = {fsr1, fsr2,fsr3,fsr4,fsr5,fsr6,fsr7};
-  byte ReturnErr[7] = {0,}; //정상 반납 여부를 저장하는 배열
+  
   while(1){
     if(checkActiv == 1){ //보관되어 있는 휴대폰의 무게를 검사하기를 활성화시킴
       for(byte i = 0; i < 7; i++){
@@ -96,51 +83,28 @@ static void vCheck(void * argl){
       }
       checkActiv = 0;
     }
-    if (runEvery(random(10000,20000))){ //10sec ~ 20sec 사이에서 임의로 전송
-      LoRaSerial.print("AT+SEND=1,11,LockerUId :");
-      LoRaSerial.print(LockerUID);
-      LoRaSerial.print("AT+SEND=1,28,");
-      Serial.print("Weight check result :");
-      for(byte i=0; i < 7, i++){
-        LoRaSerial.print(ReturnedErr[i]);
-        Serial.print(ReturnedErr[i]);
-      }
-      LoRaSerial.println();
-      LoRaSerial.println("AT+SEND=1,11,END");
-    }
+    
   }
 }
 
 //Thread3 : 문이 열렸는지 안열렸는지 판단
 static void vDoor(void *arg){
   pinMode(DOORSWITCH, INPUT); //2번에서 값 읽어옴
-  byte unlockedHolder = 0; //휴대폰 보관함이 열려있음을 알린다.
   
-  while(1){
+  while(runEvery(5000)){ //5초에 한번 읽어옴
     door = digitalRead(DOORSWITCH); //High : closed low: open
     if(door == 0){ //open
-      if(usingTime ==  0) //반납시간 이후이면
-        unlockedHolder = 1; //alert
+      door = 1;
     }
-    
-    if (runEvery(random(10000,20000))){ //10sec ~ 20sec 사이에서 임의로 전송
-      Serial.print("door : ");
-      Serial.println(door);
-      
-      LoRaSerial.print("AT+SEND=1,11,LockerUID :");
-      LoRaSerial.print(LockerUID);
-      if(unlockedHolder){
-        String messStr += "AT+SEND=1,53,"; //이곳에 gateway address 1 대신 입력
-        messStr += "휴대폰 보관함 문이 임의로 열렸습니다." //53byte
-        LoRaSerial.println(messStr);
-      }
-      LoRaSerial.println("AT+SEND=1,11,END");
+    else{
+      door = 0;
     }
   }
 }
 
-//Thread4 : 웹 -> 로라 데이터 전송
-static void vLoRa_Receive(void *arg){
+//Thread4 : 
+static void vLoRa(void *arg){
+  String InSerialStr = "";
   while(1){
     if(LoRaSerial.available()){
       String determinator = LoRaSerial.read();
@@ -156,13 +120,41 @@ static void vLoRa_Receive(void *arg){
       if(InSerialStr == "Check"){
         checkActiv = 1; //check 활성화
       }
-      // if(InSerialStr.startsWith("UsingTime:")){
-      //   usingTime = LoRaSerial.read(); //1 : 불출시작, 0: 반납시작
-      // }
       
     }
+    
+    else{ //일괄적으로 모아서 전송
+      if (runEvery(random(10000,20000))){ //10sec ~ 20sec 사이에서 임의로 전송
+      LoRaSerial.print("AT+SEND=1,11+UID_length,LockerUID :");
+      LoRaSerial.print(LockerUID);
+      
+      //RFID 전송
+      LoRaSerial.print("AT+SEND=1,32,RFID");
+      Serial.print("LoRa status: Sending message to gateway");
+      for(byte i = 0; i < 7; i++){ //불출 반납 상황 송신: 불출여부
+        LoRaSerial.print(PhoneStatus[2][i]);
+        Serial.print(PhoneStatus[2][i]);
+      }
+      //Check 
+      LoRaSerial.print("AT+SEND=1,33,Check");
+      Serial.print("Weight check result :");
+      for(byte i=0; i < 7, i++){
+        LoRaSerial.print(PhoneStatus[0][i]);
+        Serial.print(PhoneStatus[0][i]);
+      }
+      //DOOR 전송
+      if(unlockedHolder){
+        String messStr += "AT+SEND=1,53,"; //이곳에 gateway address 1 대신 입력
+        messStr += door //53byte
+        LoRaSerial.println(messStr);
+      }
+      
+      LoRaSerial.println();
+      LoRaSerial.println("AT+SEND=1,3,END");
+    
+    }
   }
-
+}
 void setup() 
 {
     Serial.begin(115200);
@@ -191,8 +183,20 @@ void setup()
     xTaskCreate(vNFC,"RFID",128, NULL, 4, &Task_NFC_Handler);
     xTaskCreate(vCheck, "Check", 64, NULL, 1, &Task_Check_Handler);
     xTaskCreate(vDoor, "DoorStatus", 64, NULL, 2, &Task_Door_Handler);
-    xTaskCreate(vLoRa_Receive, "LoRaReceive",128, NULL, 3, &Task_LoRa_Rcv_Handler);
+    xTaskCreate(vLoRa, "LoRaReceive",128, NULL, 3, &Task_LoRa_Handler);
     
 }
 
 void loop() {} //NO LOOP
+
+boolean runEvery(unsigned long interval)
+{
+  static unsigned long previousMillis = 0;
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    return true;
+  }
+  return false;
+}
